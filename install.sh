9efeb2c9c26d57f941c0d8f0ffedd2c3081b0e3d
Anserio <andershurum@gmail.com>
2018-01-04 11:44:48 +0100

    enh: made the install script a bit more intelligent

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 # Script for installing basics for awesome wm
-sudo apt install awesome
-cp .Xresources ~/
+if echo $(pwd) | grep -E */awesome-config
+then 
+  sudo apt update
+  sudo apt install awesome
+  cp ./.Xresources ~/
+  cp -r ./* ~/.config/awesome/
+  echo "-------------- INSTALL SUCCESSFUL -------------------"
+  echo "Awesome configurations from this repository installed"
+  echo "-----------------------------------------------------"
+else 
+  echo "You must cd to the directory before running this script"
+fi
